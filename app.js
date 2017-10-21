ee83dbda7e6f7dceb87896a8b34f7aff8bff1c7a
kenyon2014 <christie.cherie.kenyon@gmail.com>
2017-10-21 10:32:57 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 var Enemy = function (x, y) {
  
     this.x = x;
-    this.posX = x;
+    this.xPos = x;
 
     this.y = y;
-    this.posY = y;
+    this.yPos = y;
 
     this.sprite = "images/enemy-bug.png";
 };
@@ -17,19 +17,21 @@ var Player = function (x, y) {
     this.y = y;
 
     this.sprite = "images/char-boy.png";
-
 };
 
 //Set enemy speed, reset parameters, and collision functionality
 Enemy.prototype.update = function (dt) {
     for (var i = 0; i < allEnemies.length; i++) {
+
         allEnemies[3].speed = 100;
         allEnemies[2].speed = 100;
         allEnemies[1].speed = 100;
         allEnemies[0].speed = 100;
+
         allEnemies[6].speed = 200;
         allEnemies[5].speed = 200;
         allEnemies[4].speed = 200;
+
         allEnemies[9].speed = 150;
         allEnemies[8].speed = 150;
         allEnemies[7].speed = 150;
@@ -43,10 +45,10 @@ Enemy.prototype.update = function (dt) {
 
 //Reset enemy position when it reaches right border
 Enemy.prototype.reset = function () {
-    this.x = this.posX;
-    this.y = this.posY;
-
+    this.x = this.xPos;
+    this.y = this.yPos;
 };
+
 //Draw enemy
 Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -54,7 +56,7 @@ Enemy.prototype.render = function () {
 
 //Set player to reset when it reaches the water
 Player.prototype.update = function (dt) {
-    if (this.y < -10) {
+    if (this.y < -10 || this.x === allEnemies.x || this.y === allEnemies.y) {
         this.reset();
     }
 };
@@ -86,10 +88,10 @@ var bug3 = new Enemy(-340, 55);
 var bug2 = new Enemy(-600, 55);
 var bug1 = new Enemy(-500, 55);
 
-var allEnemies = [bug1, bug2, bug3, bug4, bug5, bug6, bug7, bug8, bug9, bug10];
+var allEnemies = [bug10, bug9, bug8, bug7, bug6, bug5, bug4, bug3, bug2, bug1];
 var player = boy;
 
-
+//Setting movement of keyboard arrow keys 
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -101,31 +103,24 @@ document.addEventListener('keyup', function(e) {
 });
 
 Player.prototype.handleInput = function (arrow) {
-    if (this.y > -20 && arrow == 'up') {
+    if (this.y > -20 && arrow == "up") {
         this.y = this.y - 25;
     }
-    if (this.y < 400 && arrow === 'down') {
+    if (this.y < 400 && arrow === "down") {
         this.y = this.y + 25;
     }
-    if (this.x < 400 && arrow === 'right') {
+    if (this.x < 400 && arrow === "right") {
         this.x = this.x + 15;
     }
-    if (this.x > 0 && arrow === 'left') {
+    if (this.x > 0 && arrow === "left") {
         this.x = this.x - 15;
     }
 };
 
-var score = {
-
-    reward: ["images/blue-gem.png", "images/green-gem.png", "Star.png"],
-    display: function () {
-        if (player.y < -10) {
-            for (var i = 0; i < score.reward.length; i++) {
-                score.reward[i]++;
-                $("#score").append(score.reward[i]);
-            }
-        }
+for (var i = 0; i < 3; i++) {
+    var score = 0;
+    if (player.y < -10) {
+        score += 100;
+        $("#score").replace("0", score);
     }
-};
-
-score.display();
+}
